3e27a419b603eb2f56bf01f7f9758f5b9e1f40e0
EMNGz <gameball326@gmail.com>
2024-02-08 16:22:45 +0700

    first commit

---
+++ error404.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error 404 - Page Not Found</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif; /* Default font family */
+            text-align: center;
+            margin: 0;
+            padding: 0;
+            background: url('backg.png') no-repeat center center fixed;
+            background-size: cover;
+            color: #00046F; /* Set text color to dark blue */
+        }
+        .row {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh; /* Set row height equal to screen height */
+        }
+        .content {
+            padding: 50px;
+            background-color: rgba(255, 255, 255, 0.7); /* Add background color for content */
+            border-radius: 10px; /* Add border radius for content */
+            margin-bottom: 250px; /* Adjust margin */
+            /* Set maximum width for content */
+            width: 100%;
+            line-height:0.5; /* Reduce line spacing */
+        }
+        h1 {
+            font-size: 200px; /* Set font size for h1 */
+            font-family: Kameron; /* Set font family for h1 */
+            color: inherit; /* Inherit color from the parent (body) */
+            margin-bottom: 0; /* Remove bottom margin */
+        }
+        p {
+            font-size: 40px; /* Set font size for p */
+            color: inherit; 
+            font-family: Kameron; /* Inherit color from the parent (body) */
+        }
+    </style>
+</head>
+<body>
+    <div class="row">
+        <div class="content">
+            <h1>Error 404 </h1>
+            <p style="font-size: 96px; padding-left :200 px;"> Page Not Found</p>
+            <p >Sorry, the page you are looking for cannot be found.</p>
+        </div>
+    </div> 
+</body>
+</html>
